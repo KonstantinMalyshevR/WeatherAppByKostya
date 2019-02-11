e60a2cb4f1d346707655ff9de54b50503233fcd0
Konstantin Malyshev <rmkonstantin@gmail.com>
2019-02-11 14:55:19 +0900

    Initial Commit

---
+++ WeatherAppByKostya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAppByKostya
+//
+//  Created by Konstantin Malyshev on 11.02.19.
+//  Copyright © 2019 Konstantin Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
